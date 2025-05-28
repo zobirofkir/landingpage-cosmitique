aee18086ab338b10f44fe9b1cc04afba64384b5d
csw <zobirofkir19@gmail.com>
2025-05-29 00:16:34 +0100

    create product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function store(ProductRequest $request)
+    {
+        Product::create($request->validated());
+        return redirect()->route('home')->with('success', 'Merci beaucoup, nous vous contacterons bientôt.');
+    }
 }
