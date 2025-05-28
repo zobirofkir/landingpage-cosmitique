ad8a411462dcaa2fc4991f4e5694f81a1f1ad21f
csw <zobirofkir19@gmail.com>
2025-05-29 00:13:02 +0100

    create product controller

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+/**
+ * Get Welcome Page
+ */
 Route::get('/', function () {
     return Inertia::render('WelcomePage');
 })->name('home');
+
+/**
+ * Store Product
+ */
+Route::post('/products', [ProductController::class, 'store'])->name("products.store");
