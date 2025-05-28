a4db986bb8abd68fc234cc9e1eb2adc343f13aa8
csw <zobirofkir19@gmail.com>
2025-05-28 17:30:14 +0100

    update

--- resources/js/pages/WelcomePage.tsx
+++ resources/js/pages/WelcomePage.tsx
@@ -42,40 +42,42 @@ const WelcomePage = () => {
         </a>
       </motion.div>
 
-      {/* Submit Button with Bounce Animation */}
-      <motion.div
-        className="fixed bottom-4 left-1/2 transform -translate-x-1/2 w-full max-w-md px-4"
-        initial={{ opacity: 0, y: 50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        <motion.button
-          type="button"
-          whileHover={{ scale: 1.05 }}
-          animate={{
-            y: [0, -4, 0],
-            boxShadow: [
-              '0 0 0px rgba(255, 165, 0, 0.5)',
-              '0 0 10px rgba(255, 165, 0, 0.8)',
-              '0 0 0px rgba(255, 165, 0, 0.5)',
-            ],
-          }}
-          transition={{
-            repeat: Infinity,
-            repeatType: 'loop',
-            duration: 2,
-            ease: 'easeInOut',
-          }}
-          className="w-full py-2 text-sm font-medium bg-orange-600 text-white rounded-md shadow-md hover:bg-orange-700 transition flex items-center justify-center gap-2 sm:py-1 sm:text-xs sm:gap-1"
-        >
-          <HiOutlineStar size={20} className="text-yellow-400 sm:size-10" />
-          <div className="flex flex-col items-center">
-            <span className="line-through text-xs text-gray-300 sm:text-xs">200 DH</span>
-            <span className="text-base font-bold sm:text-sm">170 DH</span>
-          </div>
-          <span className="sm:text-xs">إرسال | Envoyer</span>
-        </motion.button>
-      </motion.div>
+      <a href="/#products">
+           {/* Submit Button with Bounce Animation */}
+          <motion.div
+            className="fixed md:bottom-4 bottom-6 left-1/2 transform -translate-x-1/2 w-full max-w-md px-4"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <motion.button
+              type="button"
+              whileHover={{ scale: 1.05 }}
+              animate={{
+                y: [0, -4, 0],
+                boxShadow: [
+                  '0 0 0px rgba(255, 165, 0, 0.5)',
+                  '0 0 10px rgba(255, 165, 0, 0.8)',
+                  '0 0 0px rgba(255, 165, 0, 0.5)',
+                ],
+              }}
+              transition={{
+                repeat: Infinity,
+                repeatType: 'loop',
+                duration: 2,
+                ease: 'easeInOut',
+              }}
+              className="w-full py-2 text-sm font-medium bg-orange-600 text-white rounded-md shadow-md hover:bg-orange-700 transition flex items-center justify-center gap-2 sm:py-1 sm:text-xs sm:gap-1"
+            >
+              <HiOutlineStar size={20} className="text-yellow-400 sm:size-10" />
+              <div className="flex flex-col items-center">
+                <span className="line-through text-xs text-gray-300 sm:text-xs">200 DH</span>
+                <span className="text-base font-bold sm:text-sm">170 DH</span>
+              </div>
+              <span className="sm:text-xs">إرسال | Envoyer</span>
+            </motion.button>
+          </motion.div>
+      </a>
     </section>
   )
 }
