69d4185214e0a09bd0f59c94fbc77ce4dd3d4ad8
csw <zobirofkir19@gmail.com>
2025-05-29 09:43:49 +0100

    add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,8 +32,8 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="icon" href="/icon.png" sizes="any">
+        <link rel="icon" href="/icon.png" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
