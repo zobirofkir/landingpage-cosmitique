a01fa6b67b5b41e890b30c5129b1a503dbf61cab
csw <zobirofkir19@gmail.com>
2025-05-29 01:00:54 +0100

    update

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,12 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ContactRequest;
 use App\Models\Contact;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
     public function store(ContactRequest $request)
     {
-        Contact::create($request->validated());
+        $contact = Contact::create($request->validated());
+
+        /**
+         * Send email
+         */
+        Mail::raw("Name: {$contact->name}\nEmail: {$contact->email}\nMessage: {$contact->message}", function ($message) {
+            $message->to('zobirofkir19@gmail.com')
+                    ->subject('Nouvelle soumission de contact');
+        });
+
         return response()->json(['success' => 'Nous vous contacterons bientôt']);
     }
 }
