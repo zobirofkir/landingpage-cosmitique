20f2494b2e9f21ffb0fee16ac094c9e5a2810056
zobirofkir <zobirofkir19@gmail.com>
2025-06-30 11:20:51 +0200

    fix error product resource

--- app/Filament/Widgets/TableWidget.php
+++ app/Filament/Widgets/TableWidget.php
@@ -2,35 +2,6 @@
 
 namespace App\Filament\Widgets;
 
-<<<<<<< HEAD
-use Filament\Tables;
-use Filament\Tables\Table;
-use Filament\Widgets\TableWidget as BaseWidget;
-use App\Models\Product;
-use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Columns\BadgeColumn;
-
-class TableWidget extends BaseWidget
-{
-    
-    protected int|string|array $columnSpan = 'full'; 
-
-    public function table(Table $table): Table
-    {   
-        return $table
-            ->query(Product::query())
-            ->columns([
-                TextColumn::make('id')->label('ID')->sortable(),
-                TextColumn::make('name')->label('Nom du produit')->searchable(),
-                TextColumn::make('email')->label('Email du client')->searchable(),
-                TextColumn::make('phone')->label('Téléphone')->searchable(),
-                TextColumn::make('address')->label('Adresse')->limit(30),
-                TextColumn::make('quantity')->label('Quantité')->sortable(),
-                TextColumn::make('price')->label('Prix')->money('eur', true)->sortable(),
-                BadgeColumn::make('promo_code')->label('Code promo')->colors(['primary']),
-                TextColumn::make('created_at')->label('Créé le')->dateTime('d M Y H:i')->sortable(),
-            ]);
-=======
 use App\Models\Product;
 use Filament\Tables;
 use Filament\Tables\Table;
@@ -76,7 +47,7 @@ class TableWidget extends BaseWidget
                     ->sortable(),
                 TextColumn::make('price')
                     ->label('Prix')
-                    ->money('mad', true)
+                    ->money('eur', true)
                     ->sortable(),
                 BadgeColumn::make('promo_code')
                     ->label('Code promo')
@@ -105,6 +76,5 @@ class TableWidget extends BaseWidget
                     }),
             ])
             ->defaultSort('created_at', 'desc');
->>>>>>> feature/dashboard
     }
 }
