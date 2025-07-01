eed6ef201f1d7a3b0c9939866de7ef3c467dab7e
zobirofkir <zobirofkir19@gmail.com>
2025-07-01 17:19:12 +0200

    update env example and head button to create new order

--- app/Filament/Resources/ProductResource/Pages/ListProducts.php
+++ app/Filament/Resources/ProductResource/Pages/ListProducts.php
@@ -13,7 +13,7 @@ class ListProducts extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            //Actions\CreateAction::make(),
         ];
     }
 }
