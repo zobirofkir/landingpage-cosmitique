3746753d04758aaa00bc843663c6abbb6c526905
csw <zobirofkir19@gmail.com>
2025-08-21 17:28:41 +0100

    update

--- app/Http/Controllers/PhoneController.php
+++ app/Http/Controllers/PhoneController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PhoneRequest;
+use App\Models\Phone;
 use Illuminate\Http\Request;
 
 class PhoneController extends Controller
 {
-    //
+    public function store(PhoneRequest $request)
+    {
+        Phone::create($request->validated());
+        return redirect()->route('thanks');
+    }
 }
