7aef83a0d02c74f1ebe693dc6463d8f0ba62913f
csw <zobirofkir19@gmail.com>
2025-08-21 17:36:09 +0100

    create resource filament for phone

---
+++ app/Filament/Resources/PhoneResource.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PhoneResource\Pages;
+use App\Filament\Resources\PhoneResource\RelationManagers;
+use App\Models\Phone;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PhoneResource extends Resource
+{
+    protected static ?string $model = Phone::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextColumn::make("phone_number")
+                            ->label("Phone Number")
+                            ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make("phone_number")
+                            ->label("Phone Number")
+                            ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPhones::route('/'),
+            'create' => Pages\CreatePhone::route('/create'),
+            'edit' => Pages\EditPhone::route('/{record}/edit'),
+        ];
+    }
+}
